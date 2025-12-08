fd05a0655127e89608ae0b46b21ce946829367fe
Naziha <aankour.naziha@gmail.com>
2025-12-08 17:07:15 +0100

    ex02 ok

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,9 +6,78 @@
 /*   By: naankour <naankour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 17:01:45 by naankour          #+#    #+#             */
-/*   Updated: 2025/12/05 17:10:55 by naankour         ###   ########.fr       */
+/*   Updated: 2025/12/08 17:04:19 by naankour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <iostream>
+#include <exception> 
+
+class IndexException : public std::exception
+{
+public:
+    virtual const char* what() const throw()
+    {
+        return "Erreur : indice hors limites";
+    }
+};
+
+template <typename T>
+class Array
+{
+	private:
+		T	*array;
+		unsigned int _size;
+
+	public:
+		Array(): array(NULL), _size(0) {}
+
+		Array(unsigned int n): array(new T[n]()), _size(n) {}
+
+		Array(const Array<T>& other): array(NULL), _size(other._size)
+		{
+			if (_size > 0)
+			{
+				array = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+				array[i] = other.array[i];
+			}
+		}
+
+		Array<T>& operator=(const Array<T>& other)
+		{
+			if (this != &other)
+			{
+				delete[] array;
+				array = NULL;
+				_size = other._size;
+
+			if (_size > 0)
+			{
+				array = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+				array[i] = other.array[i];
+			}
+			}
+			return(*this);
+		}
+
+		~Array() 
+		{
+			delete[] array;
+		}
+
+		T& operator[](unsigned int index)
+		{
+			if (index >= _size)
+				throw IndexException();
+			return (array[index]);
+		}
+
+		unsigned int size() const
+		{
+			return (_size);
+		}
+};
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,33 @@
 /*   By: naankour <naankour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 17:01:42 by naankour          #+#    #+#             */
-/*   Updated: 2025/12/05 17:01:44 by naankour         ###   ########.fr       */
+/*   Updated: 2025/12/08 17:06:31 by naankour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int main ()
+{
+	int * a = new int();
+	std::cout << *a << std::endl;
+	std::cout << std::endl;
+
+	Array<int> b;
+	Array<int> arr(3);
+	
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[2] = 30;
+
+	std::cout << arr[0] << std::endl;
+	std::cout << arr[1] << std::endl;
+	std::cout << arr[2] << std::endl;
+	std::cout << std::endl;
+
+	int x = arr[1];
+	std::cout << x << std::endl;
+	std::cout << std::endl;
+
+	std::cout << arr.size() << std::endl;
+}
